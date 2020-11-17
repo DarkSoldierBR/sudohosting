d3424025d4e9300ef8d7481f4f63e6920f421210
DarkSoldierBR <mateus.dias029@gmail.com>
2020-11-17 20:37:55 -0300

    fix login

---
+++ controller/connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "sudo123";
+
+// Cria um conexão
+$conn = new mysqli($servername, $username, $password);
+
+// Verifica a conexão 
+if ($conn->connect_error) {
+  die("Ocorreu um erro no banco de dados do site, Informe um administrador e tente novamente mais tarde." . $conn->connect_error);
+}
+?> 
--- controller/login.php
+++ controller/login.php
@@ -8,10 +8,7 @@ if (!empty($_POST) AND (empty($_POST['email']) OR empty($_POST['password']))) {
 	header("location: /auth/login.php");
 }
 
-  // Tenta se conectar ao servidor MySQL
-mysqli_connect('localhost', 'root', 'sudo123') or trigger_error(mysql_error());
-  // Tenta se conectar a um banco de dados MySQL
-$con = mysqli_connect("localhost", "root", "sudo123", "website");
+require_once($_SERVER['DOCUMENT_ROOT'].'/controller/connect.php'); 
 
 $usuario = $_POST['email'];
 $password = $_POST['password'];
