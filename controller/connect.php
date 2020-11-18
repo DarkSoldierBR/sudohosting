11f3cd5fbc18a719995dc9cc31507696b4febe0a
DarkSoldierBR <mateus.dias029@gmail.com>
2020-11-17 22:23:04 -0300

    fix db connection

--- controller/connect.php
+++ controller/connect.php
@@ -4,10 +4,10 @@ $username = "sudohosting";
 $password = "sudo123";
 
 // Cria um conexão
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,"website");
 
 // Verifica a conexão 
-if ($conn->connect_error) {
+if ($con->connect_error) {
   die("Ocorreu um erro no banco de dados do site, Informe um administrador e tente novamente mais tarde." . $conn->connect_error);
 }
 ?> 
